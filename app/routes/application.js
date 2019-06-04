ee63e558f9afe0a76955a1d0d1339b7ebe70f525
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-06-04 10:09:41 -0400

    simplify reproduction

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,7 +2,27 @@ import Route from '@ember/routing/route';
 
 export default class ApplicationRoute extends Route {
   async model() {
-    const cryptos = await this.store.findAll('cryptocurrency');
+    const cryptos = [];
+
+    cryptos.push(this.store.createRecord('cryptocurrency', {
+      shortName: 'ETH',
+      name: 'Ethereum'
+    }));
+
+    cryptos.push(this.store.createRecord('cryptocurrency', {
+      shortName: 'BTC',
+      name: 'Bitcoin'
+    }));
+
+    cryptos.push(this.store.createRecord('cryptocurrency', {
+      shortName: 'TRX',
+      name: 'Tron'
+    }));
+
+    cryptos.push(this.store.createRecord('cryptocurrency', {
+      shortName: 'BCH',
+      name: 'Bitcoin Cash'
+    }));
 
     return { cryptos };
   }
